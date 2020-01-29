8f5319cf070def6d2ae44c36a625b3100a6eecbb
Nellie <fabybacenelson@gmail.com>
2020-01-29 15:58:24 -0700

    made public folders available

--- db/server.js
+++ db/server.js
@@ -3,11 +3,12 @@ const express = require("express");
 const app = express();
 
 const PORT = process.env.PORT || 8080;
-
+app.use(express.static("../public"));
 app.use(express.urlencoded({
     extended: true
 }));
 app.use(express.json());
+
 
 app.listen(PORT, function () {
     console.log("Note Taker listening on PORT: " + PORT);
